e0db64105a882e1a70b1754ba9a8e2f64a9746e2
chajus1 <chajus1@googlemail.com>
2024-09-08 00:01:51 +0100

    done scribling

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -4,26 +4,38 @@
     {
         static void Main(string[] args)
         {
-            /* This code tuns every 3 minutes to check the temperature.
-             * If it exceeds 160C we need to event the cooling system.
-             */
-
-            int t = m.chkTemp();
-            if (t > 160)
-            {
-                // Get the controller system for the turbines
-                T tb = new T();
-
-                //Close throttle valve on turbine #2
-                tb.clsTrpV(2);
-
-                // Fill and vent the isolation cooling system
-                ics.Fill();
-                ics.Vent();
-
-                // Initiate the air system check
-                m.airsyschk();
+            /// <summary>
+            /// If it exceeds 160C we need to event the cooling system.
+            /// </summary>
+            public bool IsNougatTooHot() {
+                int temp = CandyBarMaker.CheckNougatTemperature();
+                if (temp > 160)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
+            ///<summary>
+            /// Perform the Candy Isolation Cooling System (CICS) vent procedure.
+            /// </summary>
+            public void DoCICSVentProcedures() {                
+                TurbineController turbines = new TurbineConroller();
+                turbines.CloseTripValce(2);                
+                IsolationCoolingSystem.Fill();
+                IsolationCoolingSytem.Vent();                
+                Maker.CheckAirSystem();             
+            }
+            public void ThreeMinuteCheck()
+            {
+                if (IsNougatTooHot() == true)
+                {
+                    DoCICSVentProcedure();
+                }
+            }
+
         }
     }
 }
