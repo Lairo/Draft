e00cd263e6f2b9b15be6ceca2caf2a34e8388a1b
chajus1 <chajus1@googlemail.com>
2024-09-22 18:46:45 +0100

    adding

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -6,29 +6,44 @@
         {
             int keyA;
             int keyB;
+            
             while (true)
             {
+
+                //Console.Clear();
                 Console.Write($"Write 1st number to add to another: ");
                 string inputA = Console.ReadLine();
-                while (!int.TryParse(inputA, out keyA))
+
+                if (!int.TryParse(inputA, out keyA))
                 {
-                    Console.WriteLine("an integer please");
+                    Console.WriteLine("Enter an integer please");
                 }
+
 
                 Console.Write($"Write 2st number to add : ");
                 string inputB = Console.ReadLine();
-                while (!int.TryParse(inputB, out keyB))
+
+                if (!int.TryParse(inputB, out keyB))
                 {
-                    Console.WriteLine("an integer please");
+                    Console.WriteLine("Enter an integer please");
                 }
-                
-                Console.WriteLine($"{inputA} + {inputB} = {Addition(keyA, keyB)}");
+
+                Console.WriteLine($"{keyA} + {keyB} = {Addition(keyA, keyB)}");
+
             }
+                //Console.WriteLine("enter x to end");
+                //char end = Console.ReadKey().KeyChar;
+
+                //if (end == 'x') return;
+
             int Addition(int e, int f)
             {
-                
-                return e + f;
+                int result = e + f;
+
+
+                return result;
             }
         }
     }
 }
+
