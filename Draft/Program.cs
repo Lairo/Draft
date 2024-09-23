54a64b317e310e86ea23b0160714c43ab6390b30
chajus1 <chajus1@googlemail.com>
2024-09-23 01:33:12 +0100

    reversing?

--- Draft/Program.cs
+++ Draft/Program.cs
@@ -4,44 +4,15 @@
     {
         static void Main()
         {
-            int keyA;
-            int keyB;
-            
+
             while (true)
-            {
+            {                
 
-                //Console.Clear();
-                Console.Write($"Write 1st number to add to another: ");
+                Console.Write($"Write your string, and I will reverse it: ");
                 string inputA = Console.ReadLine();
+                if (inputA == "x") return;
 
-                if (!int.TryParse(inputA, out keyA))
-                {
-                    Console.WriteLine("Enter an integer please");
-                }
-
-
-                Console.Write($"Write 2st number to add : ");
-                string inputB = Console.ReadLine();
-
-                if (!int.TryParse(inputB, out keyB))
-                {
-                    Console.WriteLine("Enter an integer please");
-                }
-
-                Console.WriteLine($"{keyA} + {keyB} = {Addition(keyA, keyB)}");
-
-            }
-                //Console.WriteLine("enter x to end");
-                //char end = Console.ReadKey().KeyChar;
-
-                //if (end == 'x') return;
-
-            int Addition(int e, int f)
-            {
-                int result = e + f;
-
-
-                return result;
+                Console.WriteLine(inputA.Reverse().ToArray());
             }
         }
     }
